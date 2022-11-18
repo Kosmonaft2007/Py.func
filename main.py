fa921ae3f3a9bd86572a270d0fb439eb4b85c58f
Vadim <kosmonaft2007@ya.ru>
2022-11-18 22:06:24 +0300

    ii

--- main.py
+++ main.py
@@ -1,16 +1,63 @@
-# This is a sample Python script.
+from sympy import *
+from sympy.plotting import plot
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+x = Symbol("x")
+f = 5 * x ** 2 + 10 * x - 30
+f
 
+plot(f, (x, -5, 2.5))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+solve(f)
 
+solveset(f)  # 1. Определить корни
+#    Нули функции
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+solveset(f > 0)  # 2. Найти интервалы, на которых функция возрастает
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+solveset(f < 0)  # 3. Найти интервалы, на которых функция убывает  и возрастает
+
+result = [-oo, oo]  # [-oo,oo] - минус бесконечность и бесконечность
+result[1:1] = solve(diff(f), x)
+result
+
+res_increase = []
+res_dicrease = []
+for i in range(1, len(result)):
+    if is_increasing(f, Interval.open(result[i - 1], result[i])):
+        res_increase.append(f'{result[i - 1]}, {result[i]}')
+    else:
+        res_dicrease.append(f'{result[i - 1]}, {result[i]}')
+print(f'Убывает {res_dicrease}')
+print(f'Возрастает {res_increase}')
+
+# 5. Вычислить вершину
+#    Экстремумы функции
+
+result_func = solve(diff(f), x)
+for i in result_func:
+    check = f.subs(x, i)
+    if check > 0:
+        print(f'Точка минимума {i},{check}')
+    elif check < 0:
+        print(f'Точка максимума {i},{check}')
+    else:
+        print(f'{check} находится на оси')
+
+# 6. Определить промежутки, на котором f > 0
+# 7. Определить промежутки, на котором f < 0
+#    Знакопостоянства функции
+
+result = [-oo, oo]
+result[1:1] = solve(f, x)
+
+res_increase = []
+res_dicrease = []
+for i in range(1, len(result)):
+    if is_increasing(f, Interval.open(result[i - 1], result[i])):
+        res_increase.append(f'{result[i - 1]}, {result[i]}')
+    else:
+        res_dicrease.append(f'{result[i - 1]}, {result[i]}')
+print(f'f>0 {res_dicrease}')
+print(f'f<0 {res_increase}')
+
+plot(f, (x, -5, 2.5))
